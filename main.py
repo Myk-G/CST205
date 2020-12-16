2a6780f1f002d9319ee278c4fae41b2b2eae16bb
Myk-G <68793303+Myk-G@users.noreply.github.com>
2020-12-16 14:09:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,18 @@
 
 import cv2
 from bs4 import BeautifulSoup
-import urllib.request
-import urllib.error
 import requests
-import gmaps
 import geocoder
 import socket
+import base64
+
+
+def num_to_alpha(num):
+    num = hex(num)[2:].rstrip("L")
+    if len(num) % 2:
+        num = "0" + num
+
+    return base64.b64encode(num.encode())
 
 # Url is set to a specific website that contains weather data for a Highway traffic camera
 url = "https://cwwp2.dot.ca.gov/vm/js/wx/d5/sr1imjinparkway.js"
@@ -26,38 +32,26 @@ camera_data_list = camera_data.split('\n')
 # Sets variables to hold the values at their respective variable name/data
 localDate = camera_data_list[0][23:-2]
 localTime = camera_data_list[1][23:-6]
-temperatureHigh = int(camera_data_list[8][23:-2])
-temperatureLow = int(camera_data_list[9][23:-2])
+temperatureHigh = camera_data_list[8][23:-2]
+temperatureLow = camera_data_list[9][23:-2]
 sunrise = camera_data_list[10][23:-6]
 sunset = camera_data_list[11][23:-6]
 elevation = camera_data_list[12][23:-2]
 localDate = localDate[0:4] + " " + localDate[5:7] + localDate[8:]
 
-# prints the data from the website
-print("local Date = " + localDate)
-print("local time = " + localTime.translate({ord(':'): None}))
-print("temp high = ", temperatureHigh)
-print("Temp high to ASCII symbol is", hex(temperatureHigh)[2:])
-print("temp low = ", temperatureLow)
-print("Temp low to ASCII symbol is", hex(temperatureLow)[2:])
-print("sunrise = " + sunrise.translate({ord(':'): None}))
-print("sunset = " + sunset.translate({ord(':'): None}))
-print("elevation = " + elevation)
-
-gmaps.configure(api_key='AIzaSyBnyQu2Qc0so3HMhq2feR1JaZHrYjMBTjQ')
-
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 g = geocoder.ip('me')
-print(g.latlng)
-print(g.city)
+latitude = str(int(g.latlng[0] * 10000))
+longitude = str((int(g.latlng[1] * 10000)*-1))
+cityLength =  str(len(g.city))
+
+passwrd = int(latitude + localTime.translate({ord(':'): None}) + cityLength + temperatureHigh + temperatureLow + sunrise.translate({ord(':'): None}) + sunset.translate({ord(':'): None}) + elevation + longitude)
 
 
-localDate.translate({ord('-'): None}), localTime.translate({ord(':'): None}), hex(temperatureHigh)[2:], hex(temperatureLow)[2:],\
-           sunrise.translate({ord(':'): None}), sunset.translate({ord(':'): None}), elevation
-
-print(localDate.translate({ord('-'): None}), localTime.translate({ord(':'): None}), hex(temperatureHigh)[2:], hex(temperatureLow)[2:],\
-           sunrise.translate({ord(':'): None}), sunset.translate({ord(':'): None}), elevation)
+print( "Password with longitude, latitude, and weather data \n ", passwrd)
+encoded_password = num_to_alpha(passwrd)
+print("same Password encoded \n", str(encoded_password)[2:-1])
 
 
 # traffic_url = "https://cwwp2.dot.ca.gov/data/d5/cctv/image/sr1imjinparkway/sr1imjinparkway.jpg"
